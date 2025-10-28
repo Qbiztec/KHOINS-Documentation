9d8dff772092eadfcec27b5fe4d104b44fcbccff
Firrizqia <raflifirrizqiardi@gmail.com>
2025-10-28 11:11:21 +0700

    style: add blue color theme

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from "@docusaurus/preset-classic";
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: "My Site",
+  title: "Khoins API",
   tagline: "Dinosaurs are cool",
   favicon: "img/favicon.ico",
 
@@ -75,10 +75,10 @@ const config: Config = {
       respectPrefersColorScheme: true,
     },
     navbar: {
-      title: "My Site",
+      title: "Khoins-API",
       logo: {
-        alt: "My Site Logo",
-        src: "img/logo.svg",
+        alt: "Khoins Logo",
+        src: "img/logo.png",
       },
       items: [
         {
